7880f2d4bb29890801e4c6d5e2398b799162e0bc
senorrossie <dennis.roos@gmail.com>
2018-07-22 18:18:48 +0200

    Minor edits. Now loads main.html instead of showing hardcoded text.

--- index.php
+++ index.php
@@ -16,35 +16,30 @@
 		<!-- Add main styling -->
 		<link rel="stylesheet" href="css/tpl-plain.css">
 
-<?php
-include_once( "config.inc.php" );
-if( $cfg["html"]["ajax"] == "true" ) {
-	print( "<script src=\"js/ajax.js\"></script>" );
-}
-?>
+		<script src="js/ajax.js"></script>
 		<script src="js/slide.js"></script>
 		<script src="js/dragndrop.js"></script>
 	</head>
 	<body>
 		<div class="icon-bar">
-			<a class="active" href="#"><span class="glyphicon glyphicon-home"></span></a>
-			<p>&nbsp;</p>
+			<a class="active" href="<?php echo($_SELF);?>"><span class="glyphicon glyphicon-home"></span></a>
+			<br />
 			<a href="#" id="refresh" onclick="loadDoc('gen_pathtree.php', 'msg-bar');"><span class="glyphicon glyphicon-refresh"></span></a>
-			<p>&nbsp;</p>
+			<br />
 			<a href="#"><div id="eject" ondrop="eject(event)" ondragover="allowDrop(event)"><span class="glyphicon glyphicon-eject"></span></div></a>
-			<p>&nbsp;</p>
+			<br />
 			<a href="#"><div id="drive1" ondrop="drop(event)" ondragover="allowDrop(event)"><span class="glyphicon glyphicon-floppy-disk"></span></div></a>
 			<a href="#"><div id="drive2" ondrop="drop(event)" ondragover="allowDrop(event)"><span class="glyphicon glyphicon-floppy-disk"></span></div></a>
 			<a href="#"><div id="drive3" ondrop="drop(event)" ondragover="allowDrop(event)"><span class="glyphicon glyphicon-floppy-disk"></span></div></a>
 			<a href="#"><div id="drive4" ondrop="drop(event)" ondragover="allowDrop(event)"><span class="glyphicon glyphicon-floppy-disk"></span></div></a>
-			<p>&nbsp;</p>
+			<br />
 		</div>
 		<div id="msg-bar" class="hdr-msg-bar">
 		  No messages found
 		</div>
 		<div id="folder-nav" class="hdr-folder-nav">
 			<div id="folder-hdr">Folders:</div>
-			  <div id="folder-tree">
+			  <div class="folder-tree">
 					<?php
 					  if( FALSE === @include($cfg["atr"]["htmlout"]."folders.html")) {
 							include( "folders.html");
@@ -53,7 +48,11 @@ if( $cfg["html"]["ajax"] == "true" ) {
         </div>
 		</div>
 		<div id="main" class="main">
-			Use the folder bar to navigate. Drag thumbnail of disk/executable to the floppy icons to load them.
+			<?php
+				if( FALSE === @include($cfg["atr"]["htmlout"]."main.html")) {
+					include( "main.html");
+				}
+			?>
 		</div>
 	</body>
 </html>
