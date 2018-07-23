46475a31d42feb1b073fc9d2dc07d042249d6e7d
senorrossie <dennis.roos@gmail.com>
2018-07-23 23:39:09 +0200

    debug code

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -9,6 +9,18 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data).cloneNode(true));
+    msg = "Dropped a disk on " + data + "\n";
+    var drives = [ "drive1", "drive2", "drive3", "drive4"];
+    for (i = 0; i < drives.length; i++) {
+        var _el = document.getElementById(drives[i]);
+        var _hasChild = _el.hasChildNodes();
+        if (_hasChild == true ) {
+            msg += "D" + i + ": [";
+            msg += _el.firstChild.id;
+            msg += "]\n";
+        }
+    }
+    alert(msg);
 }
 function eject(ev) {
     ev.preventDefault();
