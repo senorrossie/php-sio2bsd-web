6d77eff76130ffae5274470d049ece556bd8a599
senorrossie <dennis.roos@gmail.com>
2018-07-24 21:02:24 +0200

    Trigger disk swap on inserting/ejecting floppies

--- js/dragndrop.js
+++ js/dragndrop.js
@@ -9,21 +9,14 @@ function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data).cloneNode(true));
-    msg = "Dropped a disk on " + data + "\n";
-    var drives = [ "drive1", "drive2", "drive3", "drive4"];
-    for (i = 0; i < drives.length; i++) {
-        var _el = document.getElementById(drives[i]);
-        var _hasChild = _el.hasChildNodes();
-        if (_hasChild == true ) {
-            msg += "D" + i + ": [";
-            msg += _el.firstChild.id;
-            msg += "]\n";
-        }
-    }
-    alert(msg);
+    var myFloppies = hasDisks();
+    swapDisks (myFloppies);
 }
 function eject(ev) {
     ev.preventDefault();
     var el = document.getElementById(ev.dataTransfer.getData('Text'));
     el.parentNode.removeChild(el);
+    var myFloppies = hasDisks();
+    var myFloppies = hasDisks();
+    swapDisks (myFloppies);
 }
